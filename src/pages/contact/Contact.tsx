98ffea7bb46466bcbd1ab0418127d34e79c5627f
Francisco Neto <wantedshark@gmail.com>
2023-10-02 15:38:10 -0300

    contact page completed

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,18 +1,18 @@
 export default function Contact() {
     return (
         <section className="bg-zinc-200 mt-[60px]">
-            <div className="mx-auto bg-zinc-200 max-w-[1250px] py-4 px-1 lg:px-6">
+            <div className="h-[1fr] flex flex-col justify-between mx-auto bg-zinc-200 max-w-[1250px] py-4 px-1 lg:px-6">
                 <form action="" className="bg-zinc-200 p-2 flex items-center flex-col gap-2 md:items-start">
                     <div className="w-full max-w-[400px]">
-                        <input className="bg-white p-2 w-full" type="text" placeholder="Enter your Name" />
+                        <input className="bg-white p-2 w-full outline-none" type="text" autoComplete="none" placeholder="Enter your Name" />
                     </div>
                     <div className="w-full max-w-[400px]">
-                        <input className="bg-white p-2 w-full" type="email" placeholder="Enter your Email" />
+                        <input className="bg-white p-2 w-full outline-none" type="email" autoComplete="none" placeholder="Enter your Email" />
                     </div>
                     <div className="w-full max-w-[400px] bg-white">
-                        <textarea className="bg-white min-h-[96px] max-h-24 p-2 w-full" placeholder="Enter your message" />
+                        <textarea className="bg-white min-h-[96px] max-h-24 p-2 w-full outline-none" autoComplete="none" placeholder="Enter your message" />
                     </div>
-                    <button className="mx-2 mt-3 rounded-full bg-blue-600 p-1 px-4 text-white max-[426px]:self-start">Enviar</button>
+                    <button className="mt-3 rounded-full bg-blue-600 p-1 px-4 text-white max-[426px]:self-start">Enviar</button>
                 </form>
             </div>
             <div className="w-full bg-blue-500">
